6fa78d3f3a7c36f34f3aa00a3bd8c5e99edec62c
Amazing <138585415+Amazing-Zeng@users.noreply.github.com>
2026-04-11 19:44:39 +0800

    [{Problem Topic}] {Difficulty} - {Problem Name} Time: 0 ms (100%)%, Space: 10.9 MB (67.02%)% | Language: {Solved Language}

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummyHead = new ListNode(0); // 设置一个虚拟头结点
+        dummyHead->next = head; // 将虚拟头结点指向head，这样方便后面做删除操作
+        ListNode * cur=dummyHead;
+        // 要分清楚是奇数还是偶，有顺序！！！
+        while(cur->next!=nullptr&&cur->next->next!=nullptr)
+        {
+            ListNode * temp1;
+            ListNode * temp2;
+            temp1=cur->next;
+            temp2=temp1->next->next;
+            cur->next=cur->next->next;
+            cur->next->next=temp1;
+            temp1->next=temp2;
+            cur=temp1;
+        }
+        return dummyHead->next;   
+    }
+};
