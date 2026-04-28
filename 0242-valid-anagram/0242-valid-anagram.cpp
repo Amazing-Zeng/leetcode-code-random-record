664aabc28a47f99dacfba899707b18fd11616ca4
Amazing <138585415+Amazing-Zeng@users.noreply.github.com>
2026-04-28 13:21:51 +0800

    Time: 2 ms (44.22%), Space: 9.7 MB (19.05%) - {problem_name}

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         vector<int> record(26,0);
+        if (s.size() != t.size()) return false;
         for (int i = 0; i < s.size(); i++) {
             // 并不需要记住字符a的ASCII，只要求出一个相对数值就可以了
             record[s[i] - 'a']++;
