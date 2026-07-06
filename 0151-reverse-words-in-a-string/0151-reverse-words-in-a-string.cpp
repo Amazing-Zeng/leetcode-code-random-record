3be56e02dd00ec3eec9ad07fd9bcf451e403fa49
Amazing <138585415+Amazing-Zeng@users.noreply.github.com>
2026-07-06 16:10:35 +0800

    [{Problem Topic}] {Difficulty} - {Problem Name} Time: 0 ms (100%)%, Space: 9.8 MB (68.64%)% | Language: {Solved Language}

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,48 +1,38 @@
 class Solution {
 public:
 
-    void removeExtraSpaces(string& s)
-    {
+    void removeExtraSpaces(string& s){
         int fastIndex=0,slowIndex=0;
-        for(;fastIndex<s.size();fastIndex++)
-        {
-            //遇到非空格才进行处理
-            if(s[fastIndex]!=' ')
-            {
-                if(slowIndex!=0)
-                    {
-                        s[slowIndex]=' ';
-                        slowIndex++;
-                    }
-                while(fastIndex<s.size()&& s[fastIndex]!=' ')
-                    {
-                        s[slowIndex]=s[fastIndex];
-                        slowIndex++;
-                        fastIndex++;
-                    }
-                
+        for(;fastIndex<s.size();fastIndex++){
+            if(s[fastIndex] != ' '){
+                if(slowIndex!=0){
+                    s[slowIndex++]=' ';
+                }
+                while(fastIndex<s.size()&&s[fastIndex]!=' '){
+                    s[slowIndex]=s[fastIndex];
+                    slowIndex++;
+                    fastIndex++;
+                }
             }
-
         }
         s.resize(slowIndex);
     }
+    void reverse(string& s, int start, int end){ //翻转，区间写法：左闭右闭 []
+        for(int i=start,j=end;i<j;i++,j--){
+            swap(s[i],s[j]);
+        }
+    }
+
     string reverseWords(string s) {
-        removeExtraSpaces(s); // 移除多余空格
-        reverse(s.begin(),s.end()); //翻转
-        // 翻转单词
-        int start=0; // 更新单词的位置
-        for(int i=0;i<=s.size();i++)
-        {
-            //遇到最后或空格时，翻转前面的单词
-            if(i==s.size() || s[i]==' ')
-            {
-                reverse(s.begin()+start,s.begin()+i); //reverse 是左闭右开：[begin, end)
+        removeExtraSpaces(s);
+        reverse(s,0,s.size()-1);
+        int start=0;
+        for(int i=0;i<=s.size();i++){
+            if(i==s.size()||s[i]==' '){
+                reverse(s,start,i-1);
                 start=i+1;
             }
-            
         }
         return s;
-
-        
     }
 };
