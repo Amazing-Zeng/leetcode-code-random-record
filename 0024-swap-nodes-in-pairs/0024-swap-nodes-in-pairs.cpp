e10c4ed3008db25ae68bddd78c3edbc4c9a4f484
Amazing <138585415+Amazing-Zeng@users.noreply.github.com>
2026-07-08 19:27:01 +0800

    [{Problem Topic}] {Difficulty} - {Problem Name} Time: 0 ms (100%)%, Space: 10.9 MB (94.49%)% | Language: {Solved Language}

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -11,23 +11,21 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        ListNode* dummyHead = new ListNode(0); // 设置一个虚拟头结点
-        dummyHead->next = head; // 将虚拟头结点指向head，这样方便后面做删除操作
-        ListNode * cur=dummyHead;
-        // 要分清楚是奇数还是偶，有顺序！！！
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* cur=dummy;
         while(cur->next!=nullptr&&cur->next->next!=nullptr)
         {
-            ListNode * temp1;
-            ListNode * temp2;
-            temp1=cur->next;
-            temp2=temp1->next->next; 
-            cur->next=cur->next->next;  // 步骤一
-            cur->next->next=temp1;   // 步骤二
-            temp1->next=temp2;   // 步骤三
-            cur=temp1;
+            ListNode* temp1=cur->next;
+            ListNode* temp2=temp1->next->next;
+            cur->next=cur->next->next;
+            cur->next->next=temp1;
+            temp1->next=temp2;
+
+            cur=temp1; //cur=cur->next->next;
         }
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;   
+        ListNode* result=dummy->next;
+        delete dummy;
+        return result;
     }
 };
